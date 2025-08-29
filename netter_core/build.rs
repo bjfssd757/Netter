f09f406caa8f6f37c763e9f85107a94541a7b830
Slava <545454ty@mail.ru>
2025-08-29 15:58:02 +0300

    delete netter_core/utils and netter_core/build.rs

--- netter_core/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    cc::Build::new()
-        .file("src/utils/random.c")
-        .compile("random");
-
-    println!("cargo:reran-if-changed=src/utils/random.c");
-    println!("cargo:reran-if-changed=src/utils/random.h");
-}
